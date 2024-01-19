7d97413c63e1db13b074f6a70bcc541330fe8257
jorge <yugito_is@hotmail.com>
2024-01-19 13:15:11 -0500

    Change  circuit break by retry pattern in getRoomByHotelID feature

--- src/main/java/com/jlyh/hotel/hotel/controller/HotelController.java
+++ src/main/java/com/jlyh/hotel/hotel/controller/HotelController.java
@@ -9,6 +9,7 @@ import com.jlyh.hotel.hotel.model.HotelRooms;
 import com.jlyh.hotel.hotel.model.PropertiesHotel;
 import com.jlyh.hotel.hotel.service.IHotelService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +33,8 @@ public class HotelController {
     }
 
     @GetMapping("hotels/{hotelId}")
-    @CircuitBreaker(name="getHotelByIdSupportCB", fallbackMethod = "getHotelByIdAlternative")
+    //@CircuitBreaker(name="getHotelByIdSupportCB", fallbackMethod = "getHotelByIdAlternative")
+    @Retry(name="getHotelByIdSupportRetry" , fallbackMethod = "getHotelByIdAlternative")
     public HotelRooms getHotelById(@PathVariable Long hotelId){
         return service.getHotelById(hotelId);
 
